8002074b1d836c742771a4ea6a2a512ce76f3106
Fancy-Xi <fancy921128@outlook.com>
2020-08-20 18:20:02 -0400

    revise the app settings

--- Data_product/app.R
+++ Data_product/app.R
@@ -9,18 +9,17 @@ source("ngram.R")
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Text Prediction App"),
-    p("This app predicts the next word based on the inpout phrases."),
+    titlePanel("Text Prediction Application"),
+    p("This app predicts the next word based on the input phrases."),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-                     h2("Instructions:"), 
-                     h5("1. Input a word/words in the text box."),
-                     h5("2. The predicted next word prints below it in blue."),
-                     h5("3. No need to hit enter of submit."),
-                     h5("4. A question mark means no prediction, typically due to mis-spelling"),
-                     h5("5. Additional tabs show plots of the top ngrams in the dataset"),
+                     h2("Summary"), 
+                     h5("* Input a word/words in the text box below."),
+                     h5("* The machine will tell you the next word."),
+                     h5("* When there's no model prediction, a question mark will show up."),
+                     h5("* The other tabs show the most frequent words predicted using unigram, bigram, and trigram"),
                      br(),
                      a("Source Code", href = "https://github.com/Fancy-Xi/Data-science-capstone")
                      ),
@@ -28,12 +27,22 @@ ui <- fluidPage(
         # Show a plot of the generated distribution
         mainPanel(
             tabsetPanel(
-                tabPanel("predict",
-                         textInput("user_input", h3("Your Input:"), 
+                tabPanel("Prediction",
+                         textInput("user_input", h3("What are you thinking"), 
                                    value = "Your words"),
                          h3("Predicted Next Word:"),
-                         h4(em(span(textOutput("ngram_output"), style="color:blue")))),
+                         h4(em(span(textOutput("ngram_output"), style="color:pink")))),
+                tabPanel("Unigram - Most common 20 words",
+                         br(),
+                         img(src = "unigram2.png", height = 500, width = 700)),
                 
+                tabPanel("Bigram - Most common 20 words",
+                         br(),       
+                         img(src = "biigram2.png", height = 500, width = 700)),
+                
+                tabPanel("Trigram - Most common 20 wordss",
+                         br(),
+                         img(src = "trigram2.png", height = 500, width = 700))
                 
             )   
         )
